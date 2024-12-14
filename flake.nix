25b90025835914c2ed7a0b48eda3f35f48dd9212
yannick2009 <yannickkouakou13@gmail.com>
2024-12-14 14:20:01 +0100

    Add Flake configuration files for a monorepo with Node.js and Go services

---
+++ flake.nix
@@ -0,0 +1,38 @@
+
+{
+  description = "Monorepo avec services Node.js et Go";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+
+        # Shell de développement global
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            go-tools
+            nodejs
+            just
+            hivemind
+            pnpm
+          ];
+
+          shellHook = ''
+            echo "installing dependencies..."
+            (cd "$PWD/client" && pnpm install --silent) &
+            (cd "$PWD/server" && pnpm install --silent) &
+            (cd "$PWD/pdf-service" && go mod download) &
+            wait
+            echo "🌐 Critik-V Development Environment"
+          '';
+        };
+      }
+    );
+}
